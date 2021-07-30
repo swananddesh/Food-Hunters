1634d9bfff466670f3b0d09ddca5802e65797fc8
Swanand Deshpande <swananddesh15@gmail.com>
2021-07-30 16:42:20 +0530

    Jetpack compose required dependencies are added.

--- app/src/main/java/com/stalwart/foodhunters/MainActivity.kt
+++ app/src/main/java/com/stalwart/foodhunters/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.stalwart.foodhunters
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Hey! Look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "A Button")
+                }
+            }
+        }
     }
 }
